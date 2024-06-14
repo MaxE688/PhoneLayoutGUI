81ac43da87ea918e9211e4aae075f87597d734da
MaxE688 <max.evers8@gmail.com>
2024-06-13 21:03:48 -0400

    clean up

--- src/Driver.py
+++ src/Driver.py
@@ -14,15 +14,3 @@ fm.startLoop()
 
 
 
-
-
-
-"""
-Undecided on how to handle the pages. I think creating a page only when it is 
-going to be drawn to the GUI is probably the best way to go.
-
-To delete PageFrameContainer?
-I could 
-
-
-"""
