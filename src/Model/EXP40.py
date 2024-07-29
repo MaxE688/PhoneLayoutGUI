88a1ec5d4238413988c0ffa00b6f70f5ee981143
MaxE688 <max.evers8@gmail.com>
2024-07-29 15:50:10 -0400

    update documentation

--- src/Model/EXP40.py
+++ src/Model/EXP40.py
@@ -1,19 +1,52 @@
 import tkinter as tk
 
 class EXP40:
+    """
+    used to arrange tiles in the layout for the Yealink EXP40 sidecar 
+
+    Attributes
+    ----------
+    parent : PageFrame
+        parent frame for f Frame
+    layoutManager : PageLayout
+        instance of controller that creates model layout class
+    numOfRows : int
+        the number of rows supported by exp40 model
+    f : tk.Frame
+        a frame being drawn for some reason
+
+    Methods
+    -------
+    draw(tiles : PageTile[], pageCount : int)
+        draws all of the pages elements to window
+
+    """
 
     def __init__(self, layoutManager, parent):
-
+        """
+        Parameters
+        ----------
+        layoutManager : PageLayout
+        parent : PageFrame
+        """
         self.parent = parent
         self.layoutManager = layoutManager
         self.numOfRows = 10
 
+        #TODO: why does this frame exist???
         self.f = tk.Frame(parent, width = 100)
         self.f.grid(column = 2, row = 4)
 
 
 
     def draw(self, tiles, pageCount):
+        """draws all of thes page element
+        
+        Parameters
+        ----------
+        tiles : PageTile[]
+        pageCount : int
+        """
 
         for i, tile in enumerate(tiles):
             row = i % self.numOfRows
